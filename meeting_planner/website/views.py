255112bc6cf9056171ac434e23980d50307d7e7b
Rami Burpee <rburpee@gmail.com>
2021-05-02 12:27:20 -0700

    Add date and about views to website

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def welcome(request):
     return HttpResponse("Welcome to the meeting planner!")
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("My name is Rami and I am learning Django")
