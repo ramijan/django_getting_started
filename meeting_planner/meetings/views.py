9421f1a441382842561e50d015fd2ff3671c59df
Rami Burpee <rburpee@gmail.com>
2021-05-02 14:28:41 -0700

    Welcome and detail page templates

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Meeting
+
+def detail(request, id):
+    meeting = Meeting.objects.get(pk=id)
+    return render(request, "meetings/detail.html", {"meeting": meeting})
--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,10 +1,12 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
-from datetime import datetime
+from meetings.models import Meeting
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("Welcome to the meeting planner!")
+    return render(request, "website/welcome.html",
+        {'num_meetings': Meeting.objects.count()})
 
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
