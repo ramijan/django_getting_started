055a1d08d927c92c051735822fee9e5997471fde
Rami Burpee <rburpee@gmail.com>
2021-05-02 18:17:32 -0700

    Add base template and refactor urls and add static content

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Meeting
+from .models import Meeting, Room
 
 def detail(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/detail.html", {"meeting": meeting})
+
+def rooms(request):
+    rooms = Room.objects.all()
+    return render(request, "meetings/rooms.html", {"rooms": rooms})
--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -6,7 +6,7 @@ from meetings.models import Meeting
 # Create your views here.
 def welcome(request):
     return render(request, "website/welcome.html",
-        {'num_meetings': Meeting.objects.count()})
+        {'meetings': Meeting.objects.all()})
 
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
